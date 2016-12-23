38c2e3bf5da484d9ed4b6c5b7197f83a882c1296
v-vikv <v-vikv@microsoft.com>
2016-12-23 17:42:09 +0530

    done

--- Angular2/Angular2-VersionLatest/app/Services/HttpCall.Service.ts
+++ Angular2/Angular2-VersionLatest/app/Services/HttpCall.Service.ts
@@ -1,14 +1,29 @@
-import { HttpModule } from '@angular/http';
+import { HttpModule,Http,Headers,RequestOptions } from '@angular/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+
 @Injectable()
 export class HttpCallService {
  constructor (private http: Http) {}
  httpMethodtype:string;
  Url:string;
  param:any;
-    CallHttpService()
-    {
+    CallHttpService(): Observable<any>{
+       let people$;
+       let options = new RequestOptions({ headers: headers });
+      if (this.httpMethodtype.toLocaleLowerCase()=="get") {
+        people$ = this.http
+                      .get(`${this.Url}/${this.param}`, {headers: this.getHeaders()})
+                      .map(mapPersons);
+       return people$;
+      }
+      else{
+         people$ =  
+                      .get(`${this.Url}/${this.param}`, {headers: this.getHeaders()})
+                      .map(mapPersons);
+       return people$;
+      }
 
-    } 
+   
+    }
 }
