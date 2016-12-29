79041e1348018a39ad521407dc7f05bbc1095c4f
v-vikv <v-vikv@microsoft.com>
2016-12-29 18:33:01 +0530

    added new files

--- Angular2/Angular2-VersionLatest/app/Queues/Components/QueueList.component.ts
+++ Angular2/Angular2-VersionLatest/app/Queues/Components/QueueList.component.ts
@@ -2,6 +2,7 @@ import {Component} from "@angular/core";
 import {HttpCallService} from '../../Services/HttpCall.Service'
 @Component({
     moduleId:module.id,
+    selector:"QueueList",
     templateUrl:"../Templates/QueueList.html",
 })
 export class QueueListComponent{
--- Angular2/Angular2-VersionLatest/app/Queues/Queues.module.ts
+++ Angular2/Angular2-VersionLatest/app/Queues/Queues.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule }   from '@angular/common';
 import {QueueRoutingModule} from './Queue.RouterConfig'
-
+import {HttpCallService} from '../Services/HttpCall.Service'
+import {QueueListComponent} from './Components/QueueList.component'
 @NgModule({
-    imports:      [ QueueRoutingModule]
+    imports:[ CommonModule,QueueRoutingModule],
+    declarations:[QueueListComponent,],
+    providers:[HttpCallService]
 })
 
 export class QueueModule{
-
 }
