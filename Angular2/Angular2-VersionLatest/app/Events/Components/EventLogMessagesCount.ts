2e2b76c5569c7bbae5479702261f8297b8f6d544
v-vikv <v-vikv@microsoft.com>
2017-02-23 11:53:22 +0530

    latest

--- Angular2/Angular2-VersionLatest/app/Events/Components/EventLogMessagesCount.ts
+++ Angular2/Angular2-VersionLatest/app/Events/Components/EventLogMessagesCount.ts
@@ -1,17 +1,18 @@
-import { Component,ChangeDetectionStrategy, ChangeDetectorRef,OnChanges, OnInit } from "@angular/core";
+import { Component, ChangeDetectionStrategy, ChangeDetectorRef, OnChanges, OnInit, OnDestroy } from "@angular/core";
 import { HttpCallService } from '../../Services/HttpCall.Service';
 import { ActivatedRoute, Router } from '@angular/router';
 @Component({
     moduleId: module.id,
     selector: 'event-logCount',
-    //changeDetection: ChangeDetectionStrategy.OnPush,
     templateUrl: '../Templates/EventCount.html'
 })
 
-export class EventLogCountComponent implements OnInit {
+export class EventLogCountComponent implements OnInit, OnDestroy {
     count: number;
-    message:'hello';
+    message: 'hello';
     eventLogMessages: any;
+    
+    intervalfun: any = null;
     //logMessages: any;
     error: any;
     constructor(private ref: ChangeDetectorRef, private httpService: HttpCallService, private route: ActivatedRoute, private router: Router) { }
@@ -21,20 +22,25 @@ export class EventLogCountComponent implements OnInit {
             .map(params => params)
             .do(params => this.eventLogMessages = params)
             .subscribe(id => this.getEventLogCount());
-        setInterval(() => {
-            this.getEventLogs();
-            this.ref.detectChanges();
-        }, 10000);
+        this.intervalfun = setInterval(() => {
+                this.getEventLogs();
+                this.ref.detectChanges();
+        }, 100000);
+    }
+    ngOnDestroy() {
+        if (this.intervalfun != null) {
+            clearTimeout(this.intervalfun);
+        }
     }
     getEventLogCount() {
         this.getEventLogs();
     }
     getEventLogs() {
         this.httpService.httpMethodtype = "Get";
-        this.httpService.Url = "http://localhost:64049/api/ECH/Event/GetLogsCount/" + this.eventLogMessages.logName;
+        this.httpService.Url = "http://147.243.121.91/ECHAutomation/api/ECH/Event/GetLogsCount/" + this.eventLogMessages.logName;
         this.httpService.param = this.eventLogMessages.sourceName;
         this.httpService.CallHttpService().subscribe(
-            res => this.count = res,
+            res => this.count = res ,
             error => this.error = <any>error);
     }
 }
