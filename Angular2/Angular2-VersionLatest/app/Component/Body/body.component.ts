07b6fc32ae112a8a1d283902fa674b4da83e4a60
v-vikv <v-vikv@microsoft.com>
2017-04-25 14:01:25 +0530

    Latest

--- Angular2/Angular2-VersionLatest/app/Component/Body/body.component.ts
+++ Angular2/Angular2-VersionLatest/app/Component/Body/body.component.ts
@@ -1,12 +1,22 @@
-import {Component} from "@angular/core";
-//import { HttpModule, JsonpModule } from '@angular/http';
+import { Component, OnInit } from "@angular/core";
+import { HttpCallService } from '../../Services/HttpCall.Service';
 @Component({
-   // moduleId: module.id,
-    selector:'body-component',
-    templateUrl:'/app/Templates/body_component.html'//,
+    // moduleId: module.id,
+    selector: 'body-component',
+    templateUrl: '/app/Templates/body_component.html'//,
     //providers:[HttpModule]
 })
 
-export class BodyComponent{
+export class BodyComponent implements OnInit {
+    label: string = "Outlook";
+    data: number[] = [0,0,0];
+    constructor(private httpService: HttpCallService) {
 
+    }
+
+    ngOnInit() {
+        
+    }
+
+   
 }
--- Angular2/Angular2-VersionLatest/app/SharedResource/Components/ChartComponent.ts
+++ Angular2/Angular2-VersionLatest/app/SharedResource/Components/ChartComponent.ts
@@ -1,26 +1,53 @@
-import { Component } from '@angular/core';
-import {HttpCallService} from '../../Services/HttpCall.Service'
- 
+import { Component, Input, OnInit } from '@angular/core';
+import { HttpCallService } from '../../Services/HttpCall.Service'
+
 @Component({
   selector: 'chart-component',
   templateUrl: '/app/SharedResource/Templates/chart.html'
 })
-export class ChartComponent {
+export class ChartComponent implements OnInit {
   // Doughnut
-  public doughnutChartLabels:string[] = ['Download Sales', 'In-Store Sales', 'Mail-Order Sales'];
-  public doughnutChartData:number[] = [350, 450, 100];
-  public doughnutChartType:string = 'doughnut';
+  @Input() EventName: string;
+  @Input() Hours: Number;
+  doughnutChartLabels: string[] = ["Error", "Info", "Warning"];
+  doughnutChartData: Number[] = [0, 0, 0];
+  @Input() doughnutChartType: string = 'doughnut';
 
-  constructor(private httpService:HttpCallService){
-      
+  constructor(private httpService: HttpCallService) {
   }
- 
+
+  ngOnInit() {
+    this.httpService.OpenRequest();
+    this.httpService.httpMethodtype = "Get";
+    this.httpService.Url = "api/ECH/Event/GetChartDataForLogs";
+    this.httpService.param = "/" + this.EventName + "/" + this.Hours;
+    this.httpService.CallHttpService().subscribe(
+      res => this.parseResult(res),
+      error => () => { this.httpService.CloseRequest(); });
+
+  }
+  parseResult(res: any) {
+   
+    var chartData=[0,0,0]
+    var i = 0;
+    this.doughnutChartLabels.forEach(function (data: any) {
+      var p = data;
+      res.forEach(function (d) {
+        if (p == d.name) {
+          chartData[i] = d.count;
+        }
+      })
+       i = i + 1;
+    })
+    this.doughnutChartData=chartData;
+    this.httpService.CloseRequest();
+  }
   // events
-  public chartClicked(e:any):void {
+  public chartClicked(e: any): void {
     console.log(e);
   }
- 
-  public chartHovered(e:any):void {
+
+  public chartHovered(e: any): void {
     console.log(e);
   }
 }
